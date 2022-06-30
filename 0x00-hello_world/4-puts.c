f9cb624ca3765788929e4f0a4950dbb077ab42d7
hanin77 <mohammadhanin77@gmail.com>
2022-06-30 07:06:07 -0700

    style: add missing function description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+* * main - Prints '"Programming is like building a multilingual puzzle'
+* *Return: 0 (success)
+* */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
