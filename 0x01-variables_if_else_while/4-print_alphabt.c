dd027e4a7915d08058c3abcaa4c5a9c67ed8c2d5
hanin77 <mohammadhanin77@gmail.com>
2022-07-01 18:15:24 -0700

    feat: add program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q')
+			continue;
+
+		else if (alphabet == 'e')
+			continue;
+
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
