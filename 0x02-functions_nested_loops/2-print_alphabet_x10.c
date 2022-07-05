4e6e7e3b04eba6a0dda2f274f2803ba3a92ecb3d
hanin77 <mohammadhanin77@gmail.com>
2022-07-05 09:45:15 -0700

    feat: add function that prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints the alphabet 10 times
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
